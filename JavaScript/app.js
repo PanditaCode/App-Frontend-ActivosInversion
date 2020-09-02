ec49dd816042da800656209124630cef319b20c3
DevPandita <51542657+PanditaCode@users.noreply.github.com>
2020-09-02 16:49:38 -0500

    Add files via upload

---
+++ JavaScript/app.js
@@ -0,0 +1,11 @@
+const form__btn = document.querySelector('#form__btn');
+const form__input = document.querySelector("#form__input");
+const login__container = document.querySelector("#login__container");
+const icons_progress = document.querySelector("#icons_progress");
+const step_progressbar = document.querySelector("#step_progressbar");
+
+form__btn.addEventListener('click', () =>){
+    login__container.classList.add("login__container-mode");
+});
+
+
